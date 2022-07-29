dc9c0c5cbaa787fb88ed9f9def162d8e59ee0cbd
manab oz <staff@oz-edu.com>
2022-07-29 17:33:41 +0900

    第2回まで

---
+++ Landmarks/LandmarkList.swift
@@ -0,0 +1,27 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by 桝谷希映 on 2022/07/29.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)){
+                  LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
--- Landmarks/Models/Landmark.swift
+++ Landmarks/Models/Landmark.swift
@@ -1,12 +1,36 @@
-//
-//  Landmark.swift
-//  Landmarks
-//
-//  Created by 桝谷希映 on 2022/07/29.
-//
-
 import SwiftUI
+import CoreLocation
 
-struct Landmark {
+struct Landmark: Hashable, Codable, Identifiable{
+    var id: Int
+    var name: String
+    fileprivate var imageName: String
+    fileprivate var coordinates: Coordinates
+    var state: String
+    var park: String
+    var category: Category
     
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+    
+    enum Category: String, CaseIterable, Hashable, Codable{
+        case featured = "Featured"
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+        case mountains = "Mountains"
+    }
+}
+
+extension Landmark {
+    var image: Image {
+        ImageStore.shared.image(name: imageName)
+    }
+}
+
+struct Coordinates: Hashable, Codable {
+    var latitude: Double
+    var longitude: Double
 }
